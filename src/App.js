5d2c5b947c9e6a44020830ffad14ae47777423c6
vedangj044 <vedangj044@gmail.com>
2021-04-30 16:53:59 +0530

    Websockets

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Paper from "./components/paper";
 import Card from "./components/card";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 function App() {
 
   const [info, setInfo] = useState([]);
+  const ws = useRef(null);
 
   const deleteElement = iden => {
     setInfo(info.filter(item => item.id !== iden))
@@ -12,14 +13,23 @@ function App() {
 
   useEffect(() => {
 
-    setInfo([
-      {id: "s" + (Math.floor(Math.random() * 10000) + 1), bed: "10", type: "1"},
-      {id: "s" + (Math.floor(Math.random() * 10000) + 1), bed: "13", type: "1"},
-      {id: "s" + (Math.floor(Math.random() * 10000) + 1), bed: "12", type: "1"},
-      {id: "s" + (Math.floor(Math.random() * 10000) + 1), bed: "16", type: "2"},
-    ]);
+    ws.current = new WebSocket("wss://mangovest.herokuapp.com/realtime");
+    ws.current.onopen = () => console.log("Connection opened");
+    ws.current.onclose = () => console.log("Connection closed");
+    
+    return () => {
+      ws.current.close();
+    };
+  }, []);
 
-  }, [])
+  useEffect(() => {
+    if (!ws.current) return;
+    ws.current.onmessage = e => {
+      const message = JSON.parse(e.data);
+      console.log("e", message);
+      setInfo([...info, {id: "s" + (Math.floor(Math.random() * 10000) + 1), bed: message.bed, type: message.command}]);
+    }
+  }, [info]);
 
   return (
     <div className="mx-0 xl:mx-40">
